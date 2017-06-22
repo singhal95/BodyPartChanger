6bcd7140eb794a1f157cd852219ba38a38b4f131
nitin singhal <singhal95nitin@gmail.com>
2017-06-22 10:18:52 +0530

    fragment is added

--- app/src/main/java/com/example/android/android_me/BodyPartFragment.java
+++ app/src/main/java/com/example/android/android_me/BodyPartFragment.java
@@ -8,6 +8,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
+import com.example.android.android_me.data.AndroidImageAssets;
+
 /**
  * Created by NITIN on 22-Jun-17.
  */
@@ -21,6 +23,7 @@ public class BodyPartFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view=inflater.inflate(R.layout.fragment_body_part,null);
         ImageView imageView= (ImageView) view.findViewById(R.id.body_part);
+        imageView.setImageResource(AndroidImageAssets.getHeads().get(0));
 
         return view;
     }
